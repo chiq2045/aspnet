3d71f5989e79208a359dd185fb0bd7ae95ea9c6d
chiq2045 <charles.oroko@outlook.com>
2019-10-21 11:12:55 +0000

    add a model and views for model
    also connect model to local database using SQLite

--- Data/MovieAppContext.cs
+++ Data/MovieAppContext.cs
@@ -3,12 +3,12 @@ using aspnet.Models;
 
 namespace aspnet.Data
 {
-  public class MovieAppContext : DbContext
-  {
-    public MovieAppContext (DbContextOptions<MovieAppContext> options) : base(options)
+    public class MovieAppContext : DbContext
     {
-    }
+        public MovieAppContext (DbContextOptions<MovieAppContext> options) : base(options)
+        {
+        }
 
-    public DbSet<Movie> Movie { get; set; }
-  }
+        public DbSet<Movie> Movie { get; set; }
+    }
 }
