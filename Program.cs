5edccb6e9c4b9fe54de48705f6216e4d4276320c
chiq2045 <charles.oroko@outlook.com>
2019-10-21 11:26:14 +0000

    seed database

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using aspnet.Data;
+using aspnet.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace aspnet
 {
@@ -13,7 +16,24 @@ namespace aspnet
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+            host.Run();
+            /* CreateHostBuilder(args).Build().Run(); */
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
